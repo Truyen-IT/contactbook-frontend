ccd850e157ae83ea1de3479e4bb6c94d36f32d71
Truyen-IT <truyenb1910323@student.ctu.edu.vn>
2022-04-06 21:49:52 +0700

    test

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Oopss, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
